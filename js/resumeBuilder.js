7daa8d407f152a5ec206d0072089480dc7d5e7c9
Jropp <jasonropp4tl@gmail.com>
2017-08-29 13:46:09 -0400

    display functions changed to forEach. Page is working but still lacking information needed in online classes objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,62 +6,62 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 //OBJECTS STORING BIO INFORMATION EDUCATION AND WORK HISTORY
 var bio = {
-  "name": "Jason Ropp",
-  "role": "JavaScript Programmer",
-  "contacts": {
-      "mobile": "541-619-4586",
-      "email": "jasonropp@jasonropp.com",
-      "github": "@jropp",
-      "twitter": "@jason__ropp",
-      "location": "Goshen, IN"
-  },
-  "welcomeMessage": "Hey there amigos!",
-  "skills": ["JavaScript", "Effective Writing", "Creativity"],
-  "image": "images/me.jpg",
-  "display": function() { //appends biopic, name, skills, and contact information to header as well as the contact information to the footer. 
-      var formattedBioPic = HTMLbioPic.replace("%data%", bio.image);
-      $("#header").append(formattedBioPic);
+	"name": "Jason Ropp",
+	"role": "JavaScript Programmer",
+	"contacts": {
+		"mobile": "541-619-4586",
+		"email": "jasonropp@jasonropp.com",
+		"github": "@jropp",
+		"twitter": "@jason__ropp",
+		"location": "Goshen, IN"
+	},
+	"welcomeMessage": "Hey there amigos!",
+	"skills": ["JavaScript", "Effective Writing", "Creativity"],
+	"image": "images/me.jpg",
+	"display": function() { //appends biopic, name, skills, and contact information to header as well as the contact information to the footer. 
+		var formattedBioPic = HTMLbioPic.replace("%data%", bio.image);
+		$("#header").append(formattedBioPic);
 
-      var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name); //TODO: each of these needs to replace "#" with a link to the appropriate site.
-      $("#header").append(formattedHeaderName);
-      if (bio.skills.length > 0) {
-          $("#header").append(HTMLskillsStart);
-      }
-      var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
-      $("#skills").append(formattedSkills);
-      var formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
-      $("#skills").append(formattedSkills);
+		var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name); //TODO: each of these needs to replace "#" with a link to the appropriate site.
+		$("#header").append(formattedHeaderName);
+		if (bio.skills.length > 0) {
+			$("#header").append(HTMLskillsStart);
+		}
+		var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkills);
+		var formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
+		$("#skills").append(formattedSkills);
 
-      var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
-      $("#topContacts").append(formattedMobile);
+		var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+		$("#topContacts").append(formattedMobile);
 
-      var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
-      $("#footerContacts").append(formattedMobile);
+		var formattedMobile = HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+		$("#footerContacts").append(formattedMobile);
 
-      var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
-      $("#topContacts").append(formattedEmail);
+		var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+		$("#topContacts").append(formattedEmail);
 
-      var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
-      $("#footerContacts").append(formattedEmail);
+		var formattedEmail = HTMLemail.replace("%data%", bio.contacts["email"]);
+		$("#footerContacts").append(formattedEmail);
 
-      var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
-      $("#topContacts").append(formattedTwitter);
+		var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+		$("#topContacts").append(formattedTwitter);
 
-      var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
-      $("#footerContacts").append(formattedTwitter);
+		var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+		$("#footerContacts").append(formattedTwitter);
 
-      var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
-      $("#topContacts").append(formattedGithub);
+		var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
+		$("#topContacts").append(formattedGithub);
 
-      var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
-      $("#footerContacts").append(formattedGithub);
+		var formattedGithub = HTMLgithub.replace("%data%", bio.contacts["github"]);
+		$("#footerContacts").append(formattedGithub);
 
-      var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
-      $("#topContacts").append(formattedLocation);
+		var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+		$("#topContacts").append(formattedLocation);
 
-      var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
-      $("#footerContacts").append(formattedLocation);
-  }
+		var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+		$("#footerContacts").append(formattedLocation);
+	}
 }
 bio.display();
 
@@ -69,180 +69,190 @@ bio.display();
 
 
 var education = { //FIXME it displays the containers but says "undefined for all of them"
-  "schools": [{
-      "name": "Rosedale Bible College",
-      "location": "Rosedale, Ohio",
-      "degree": "AA",
-      "majors": ["Pastoral Studies"],
-      "dates": "2005–2007",
-      "url": "www.rosedale.edu"
-  }, {
-      "name": "Grace College",
-      "location": "Winona Lake",
-      "degree": "BA",
-      "majors": ["English", " Biblical Studies"],
-      "dates": "2009–2012",
-      "url": "www.grace.edu"
-  }, {
-      "name": "Udacity", //TODO Move this to the top of the order
-      "location": "Goshen",
-      "degree": "Front End Developer Nano-Degree",
-      "majors": ["JavaScript", " HTML", " CSS"],
-      "dates": "2017",
-      "url": "www.udacity.com"
-  }],
-  "onlineCourses": [{ //TODO: need all the courses listed
-      "title": "$", //TODO need title for each course.
-      "school": "Udacity", //TODO: enter freecodecamp into this
-      "dates": "2017",
-      "url": "www.udacity.com"
-  }],
-  "display": function() {
+	"schools": [{
+		"name": "Rosedale Bible College",
+		"location": "Rosedale, Ohio",
+		"degree": "AA",
+		"majors": ["Pastoral Studies"],
+		"dates": "2005–2007",
+		"url": "www.rosedale.edu"
+	}, {
+		"name": "Grace College",
+		"location": "Winona Lake",
+		"degree": "BA",
+		"majors": ["English", " Biblical Studies"],
+		"dates": "2009–2012",
+		"url": "www.grace.edu"
+	}, {
+		"name": "Udacity", //TODO Move this to the top of the order
+		"location": "Goshen",
+		"degree": "Front End Developer Nano-Degree",
+		"majors": ["JavaScript", " HTML", " CSS"],
+		"dates": "2017",
+		"url": "www.udacity.com"
+	}],
+	"onlineCourses": [{ //TODO: need all the courses listed
+			"title": "HTML5 and CSS", //TODO need title for each course.
+			"school": "Free Code Camp", //TODO: enter freecodecamp into this
+			"dates": "2017",
+			"url": "www.freecodecamp.com"
+		},
+		{ //TODO: need all the courses listed
+			"title": "Responsive Design with Bootstrap", //TODO need title for each course.
+			"school": "Free Code Camp", //TODO: enter freecodecamp into this
+			"dates": "2017",
+			"url": "www.freecodecamp.com"
+		},
+		{ //TODO: need all the courses listed
+			"title": "jQuery", //TODO need title for each course.
+			"school": "Free Code Camp", //TODO: enter freecodecamp into this
+			"dates": "2017",
+			"url": "www.freecodecamp.com"
+		}
 
-      // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-      // replace the for...in statements with the for statement or the Array forEach() method
-      for (school in education.schools) {
-          $("#education").append(HTMLschoolStart); //FIXME needs to be moved to title the courses rather than be all the way at the end.
-          var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
-          $(".education-entry:last").append(formattedSchoolName);
+		//TODO: DO I list these all as separate courses or just finish out and name the Front End Development Certification?
 
-          var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-          $(".education-entry:last").append(formattedSchoolDegree);
-
-          var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-          $(".education-entry:last").append(formattedSchoolDates);
-
-          var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-          $(".education-entry:last").append(formattedSchoolLocation);
-
-          var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
-          $(".education-entry:last").append(formattedSchoolMajor);
-
-          //TODO: the "onlineCourses" array also needs a round of functions"
-
-      } //end of education loop
-
-      $("#education").append(HTMLonlineClasses);
-
-      for (course in education.onlineCourses) {
-          $("#education").append(HTMLschoolStart);
-
-          var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title)
-          $(".education-entry:last").append(formattedTitle);
-          var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school)
-          $(".education-entry:last").append(formattedSchool);
-          var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates)
-          $(".education-entry:last").append(formattedDates);
-          var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url)
-          $(".education-entry:last").append(formattedURL);
+	],
+	"display": function() {
 
 
-      } //end of online courses loop
+		education.schools.forEach(function(school) {
+			$("#education").append(HTMLschoolStart); //FIXME needs to be moved to title the courses rather than be all the way at the end.
+			var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
+			$(".education-entry:last").append(formattedSchoolName);
 
-  } //end of display function
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+			$(".education-entry:last").append(formattedSchoolDegree);
+
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+			$(".education-entry:last").append(formattedSchoolDates);
+
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+			$(".education-entry:last").append(formattedSchoolLocation);
+
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
+			$(".education-entry:last").append(formattedSchoolMajor);
+
+
+		});
+
+		$("#education").append(HTMLonlineClasses);
+
+		education.onlineCourses.forEach(function(course) {
+			$("#education").append(HTMLschoolStart);
+
+			var formattedTitle = HTMLonlineTitle.replace("%data%", course.title)
+			$(".education-entry:last").append(formattedTitle);
+			var formattedSchool = HTMLonlineSchool.replace("%data%", course.school)
+			$(".education-entry:last").append(formattedSchool);
+			var formattedDates = HTMLonlineDates.replace("%data%", course.dates)
+			$(".education-entry:last").append(formattedDates);
+			var formattedURL = HTMLonlineURL.replace("%data%", course.url)
+			$(".education-entry:last").append(formattedURL);
+
+
+		}) //end of online courses loop
+
+	} //end of display function
 }
 
 education.display();
 
 var work = {
 
-  "jobs": [{
-      "title": "Guitar Teacher",
-      "employer": "Self-Employed",
-      "dates": "2009-2017",
-      "description": "Gave 8,000+ lessons to 150+ different students. I built my business almost entirely by word of mouth.",
-      "location": "Goshen, Indiana"
-  }, {
-      "title": "Espresso Bar Catering Manager",
-      "employer": "The Electric Brew",
-      "dates": "2014-2016",
-      "description": "I ran the espresso bar catering as my own business. I set up and made drinks at events, organized other employees for events and marketed the business. I was also in charge of keeping track of inventory, income and expense.",
-      "location": "Goshen, Indiana"
-  }, {
-      "title": "Coffee Roaster",
-      "employer": "The Electric Brew",
-      "dates": "2010-2015",
-      "description": "Roasted 10,000lbs per year of coffee for The Electric Brew",
-      "location": "Goshen, Indiana",
+	"jobs": [{
+		"title": "Guitar Teacher",
+		"employer": "Self-Employed",
+		"dates": "2009-2017",
+		"description": "Gave 8,000+ lessons to 150+ different students. I built my business almost entirely by word of mouth.",
+		"location": "Goshen, Indiana"
+	}, {
+		"title": "Espresso Bar Catering Manager",
+		"employer": "The Electric Brew",
+		"dates": "2014-2016",
+		"description": "I ran the espresso bar catering as my own business. I set up and made drinks at events, organized other employees for events and marketed the business. I was also in charge of keeping track of inventory, income and expense.",
+		"location": "Goshen, Indiana"
+	}, {
+		"title": "Coffee Roaster",
+		"employer": "The Electric Brew",
+		"dates": "2010-2015",
+		"description": "Roasted 10,000lbs per year of coffee for The Electric Brew",
+		"location": "Goshen, Indiana",
 
-  }],
+	}],
 
-  "display": function() {
+	"display": function() {
 
-      for (job in work.jobs) {
-          $("#workExperience").append(HTMLworkStart);
+		work.jobs.forEach(function(job) {
+			$("#workExperience").append(HTMLworkStart);
 
-          var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-          var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-          var formattedEmployerTitle = formattedEmployer + formattedTitle;
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+			var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-          $(".work-entry:last").append(formattedEmployerTitle);
-
-
-          var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-          $(".work-entry:last").append(formattedWorkDates);
-
-          var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-          $(".work-entry:last").append(formattedWorkDescription);
-
-          var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-          $(".work-entry:last").append(formattedWorkLocation);
+			$(".work-entry:last").append(formattedEmployerTitle);
 
 
-      }
-  }
+			var formattedWorkDates = HTMLworkDates.replace("%data%", job.dates);
+			$(".work-entry:last").append(formattedWorkDates);
 
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", job.description);
+			$(".work-entry:last").append(formattedWorkDescription);
 
-
+			var formattedWorkLocation = HTMLworkLocation.replace("%data%", job.location);
+			$(".work-entry:last").append(formattedWorkLocation);
+		});
+	}
 }
 
 work.display();
 
 var projects = {
-  "projects": [{
-      "title": "Project 1",
-      "dates": "2017",
-      "description": "This is a placeholder for the project that will be",
-      "images": ["images/fry.jpg"]
-  }, {
-      "title": "Project 2",
-      "dates": "2017",
-      "description": "This is a placeholder for the project that will be",
-      "images": ["images/fry.jpg"]
-  }, {
-      "title": "yelling contest",
-      "dates": "2017",
-      "description": "This is a placeholder for the project that will be",
-      "images": ["images/fry.jpg"]
-  }],
+	"projects": [{
+		"title": "Project 1",
+		"dates": "2017",
+		"description": "This is a placeholder for the project that will be",
+		"images": ["images/fry.jpg"]
+	}, {
+		"title": "Project 2",
+		"dates": "2017",
+		"description": "This is a placeholder for the project that will be",
+		"images": ["images/fry.jpg"]
+	}, {
+		"title": "yelling contest",
+		"dates": "2017",
+		"description": "This is a placeholder for the project that will be",
+		"images": ["images/fry.jpg"]
+	}],
 
-  "display": function() {
+	"display": function() {
 
-      for (project in projects.projects) {
+		projects.projects.forEach(function(project) {
 
-          $("#projects").append(HTMLprojectStart);
+			$("#projects").append(HTMLprojectStart);
 
-          var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-          $(".project-entry:last").append(formattedTitle);
+			var formattedTitle = HTMLprojectTitle.replace("%data%", project.title);
+			$(".project-entry:last").append(formattedTitle);
 
-          var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-          $(".project-entry:last").append(formattedDates);
+			var formattedDates = HTMLprojectDates.replace("%data%", project.dates);
+			$(".project-entry:last").append(formattedDates);
 
-          var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-          $(".project-entry:last").append(formattedDescription);
+			var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
+			$(".project-entry:last").append(formattedDescription);
 
-          if (projects.projects[project].images.length > 0) {
-              for (image in projects.projects[project].images) {
-                  var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-                  $(".project-entry:last").append(formattedImage);
+			if (project.images.length > 0) {
+				project.images.forEach(function(image) {
+
+					var formattedImage = HTMLprojectImage.replace("%data%", image);
+					$(".project-entry:last").append(formattedImage);
 
 
-              }
-          }
-      }
+				}); //end of for in if
+			} //end of if
+		}); //end of for each function
 
-  }
-}
+	} //end of display function
+} //end of object
 
 projects.display();
 
